42b65666a76f476356880cdece0398b60de48648
Adi <adrian.etegan@nordlogic.com>
2021-12-24 11:46:56 +0200

    Try using application_credentials to get token

--- KeystoneV3Authenticator.js
+++ KeystoneV3Authenticator.js
@@ -1,104 +1,102 @@
-"use strict";
+'use strict';
 
 const EventEmitter = require('events');
 const requestp = require('request-promise');
 
 class KeystoneV3Authenticator extends EventEmitter {
-  constructor(credentials) {
-    super();
+    constructor(credentials) {
+        super();
 
-    this.credentials = credentials;
-    this.currentToken = null;
-  }
-
-  tryFindEndpointUrl(catalog, service, iface, regionId) {
-    if (typeof iface === 'undefined') {
-      iface = "public";
+        this.credentials = credentials;
+        this.currentToken = null;
     }
 
-    const catalogEntry = catalog.find(x => x.name === service);
-    if (!catalogEntry) {
-      return null;
-    }
-
-    const endpoint = catalogEntry.endpoints.find(x => x.interface === iface && (regionId ? x.region_id == regionId : true));
-    if (!endpoint) {
-      return null;
-    }
-
-    return endpoint.url;
-  }
-
-  async getToken() {
-    const credentials = this.credentials;
-    const model = {
-      auth: {
-        identity: {
-          methods: [
-            'password'
-          ],
-          password: {
-            user: {
-              name: credentials.username,
-              password: credentials.password,
-              domain: {
-                id: credentials.domainId
-              }
-            }
-          }
-        },
-        scope: {
-          project: {
-            id: credentials.projectId,
-            domain: {
-              id: credentials.domainId
-            }
-          }
+    tryFindEndpointUrl(catalog, service, iface, regionId) {
+        if (typeof iface === 'undefined') {
+            iface = 'public';
         }
-      }
+
+        const catalogEntry = catalog.find(x => x.name === service);
+        if (!catalogEntry) {
+            return null;
+        }
+
+        const endpoint = catalogEntry.endpoints.find(x => x.interface === iface && (regionId ? x.region_id == regionId : true));
+        if (!endpoint) {
+            return null;
+        }
+
+        return endpoint.url;
+    }
+
+    async getToken() {
+        const credentials = this.credentials;
+        const model = {
+            auth: {
+                identity: {
+                    methods: [
+                        'application_credentials'
+                    ],
+                    application_credentials: {
+                        id: credentials.applicationId,
+                        domain: {
+                            id: credentials.domainId || 'default'
+                        },
+                        secret: credentials.applicationSecret,
+                    }
+                },
+                scope: {
+                    project: {
+                        id: credentials.projectId,
+                        domain: {
+                            id: credentials.domainId
+                        }
+                    }
+                }
+            }
+        };
+
+        const response = await requestp({
+            method: 'POST',
+            uri: credentials.endpointUrl + '/auth/tokens',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            json: model,
+            resolveWithFullResponse: true
+        });
+
+        const catalog = response.body.token.catalog;
+        const swiftUrl =
+            this.tryFindEndpointUrl(catalog, 'swift', credentials.endpointUrlInterface, credentials.regionId)
+            || this.tryFindEndpointUrl(catalog, 'radosgw-swift', credentials.endpointUrlInterface, credentials.regionId); // many OpenStack clouds use ceph radosgw to provide swift
+
+        if (!swiftUrl) {
+            throw new Error('could not find swift or radosgw-swift service in catalog');
+        }
+
+        return {
+            token: response.headers['x-subject-token'],
+            expires: new Date(response.body.token.expires_at), // expires_at is an ISO 8601 Date:
+            swiftUrl: swiftUrl
+        }
     };
 
-    const response = await requestp({
-      method: 'POST',
-      uri: credentials.endpointUrl + '/auth/tokens',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      json: model,
-      resolveWithFullResponse: true
-    });
+    async authenticate() {
+        if (!this.currentToken) {
+            this.currentToken = await this.getToken();
+        }
 
-    const catalog = response.body.token.catalog;
-    const swiftUrl =
-      this.tryFindEndpointUrl(catalog, 'swift', credentials.endpointUrlInterface, credentials.regionId)
-      || this.tryFindEndpointUrl(catalog, 'radosgw-swift', credentials.endpointUrlInterface, credentials.regionId); // many OpenStack clouds use ceph radosgw to provide swift
+        const tokenPreRefreshInterval = 10000; // renew tokens 10s before they expire
+        const requestedTokenExpiry = new Date(Date.now() + tokenPreRefreshInterval)
+        if (requestedTokenExpiry > this.currentToken.expires) {
+            this.currentToken = await this.getToken();
+        }
 
-    if (!swiftUrl) {
-      throw new Error('could not find swift or radosgw-swift service in catalog');
+        const validToken = this.currentToken;
+        return {url: validToken.swiftUrl, token: validToken.token};
     }
-
-    return {
-      token: response.headers['x-subject-token'],
-      expires: new Date(response.body.token.expires_at), // expires_at is an ISO 8601 Date:
-      swiftUrl: swiftUrl
-    }
-  };
-
-  async authenticate() {
-    if (!this.currentToken) {
-      this.currentToken = await this.getToken();
-    }
-
-    const tokenPreRefreshInterval = 10000; // renew tokens 10s before they expire
-    const requestedTokenExpiry = new Date(Date.now() + tokenPreRefreshInterval)
-    if (requestedTokenExpiry > this.currentToken.expires) {
-      this.currentToken = await this.getToken();
-    }
-
-    const validToken = this.currentToken;
-    return { url: validToken.swiftUrl, token: validToken.token };
-  }
 }
 
 module.exports = KeystoneV3Authenticator;
